74810f7103af8a207ee96bca71229b429c7615a9
Abir <amabirmahmud@gmail.com>
2025-07-13 17:35:43 +0600

    Update StudentDTO.cs

--- SchoolAPI/DTOs/StudentDTO.cs
+++ SchoolAPI/DTOs/StudentDTO.cs
@@ -1,15 +1,9 @@
-﻿namespace SchoolAPI.DTOs
+﻿using SchoolAPI.Models;
+
+namespace SchoolAPI.DTOs
 {
-    public class StudentDTO
+    public class StudentDTO: Student
     {
-        public int StudentId { get; set; }
-
-        public required string Name { get; set; }
-        public int Age { get; set; }
-
-        public int SchoolId { get; set; }
-        public int ClassId { get; set; }
-
         public string SchoolName { get; set; }
         public string SchoolAddress { get; set; }
         public string ClassName { get; set; }
